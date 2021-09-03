303829229424409572404cf944f4861ca741ba35
coffmanjrp <coffmanjrp@gmail.com>
2021-09-03 16:48:34 -0700

    Modify cookie data with fetch

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,12 +1,21 @@
 import { Magic } from '@magic-sdk/admin';
-
-let magic = new Magic(process.env.MAGIC_SECRET_KEY);
+import Iron from '@hapi/iron';
+import CookieService from '../../lib/cookie';
 
 const login = async (req, res) => {
   if (req.method !== 'POST') return res.status(405).end();
 
-  const did = magic.utils.parseAuthorizationHeader(req.headers.authorization);
-  const user = await magic.users.getMetadataByToken(did);
+  const did = req.headers.authorization.split('Bearer').pop().trim();
+  const user = await new Magic(
+    process.env.MAGIC_SECRET_KEY
+  ).users.getMetadataByToken(did);
+
+  const token = await Iron.seal(
+    user,
+    process.env.ENCRYPTION_SECRET,
+    Iron.defaults
+  );
+  CookieService.setTokenCookie(res, token);
 
   res.end();
 };
